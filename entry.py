bec8fbd1d6761377d2b1e44d3ccdebf77ca69501
Isaac Spencer <22ispencer@gmail.com>
2024-11-28 17:33:42 -0600

    creating cross sections and slurm entrypoint

---
+++ entry.py
@@ -0,0 +1,22 @@
+import itertools
+from numba import njit
+import numpy as np
+
+MIN_STRINGER_COUNT = 3
+MAX_STRINGER_COUNT = 10
+STRINGER_PLACEMENT_STEP = 1 / 8
+
+
+def cross_sections(min_stringer_count: int, max_stringer_count: int):
+    positions = np.arange(0, 8, STRINGER_PLACEMENT_STEP)
+
+    cross_sections = []
+    for stringer_num in range(min_stringer_count, max_stringer_count):
+        combinations = list(itertools.combinations(positions, r=stringer_num))
+        cross_sections.extend(combinations)
+
+    return cross_sections
+
+
+if __name__ == "__main__":
+    print(len(cross_sections(MIN_STRINGER_COUNT, MAX_STRINGER_COUNT)))
