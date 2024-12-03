f8d413a2aac1293cdcef6f58e41f4ee5826d3f13
Isaac Spencer <22ispencer@gmail.com>
2024-12-02 18:42:17 -0600

    wip

--- entry.py
+++ entry.py
@@ -1,6 +1,8 @@
 import itertools
 import numpy as np
 
+import solve
+
 MIN_STRINGER_COUNT = 5
 MAX_STRINGER_COUNT = 10
 STRINGER_PLACEMENT_STEP = 1 / 8
@@ -10,14 +12,48 @@ def cross_sections(min_stringer_count: int, max_stringer_count: int):
     positions = np.arange(0, 8, STRINGER_PLACEMENT_STEP)
 
     for stringer_num in range(min_stringer_count, max_stringer_count + 1):
-        for c in itertools.combinations(positions, r=stringer_num):
-            yield c
+        yield itertools.combinations(positions, r=stringer_num)
 
 
 if __name__ == "__main__":
-    count = 0
-    for batch in itertools.batched(
-        cross_sections(MIN_STRINGER_COUNT, MAX_STRINGER_COUNT), n=1024
-    ):
-        count += len(batch)
-    print(count)
+    num = 5
+    for x_sections in cross_sections(num, num):
+        cs_array = np.array(list(x_sections))
+        props = np.zeros((cs_array.shape[0], cs_array.shape[1]))
+        solve.section_properties(cs_array, props)
+
+        step = 50
+
+        loads = np.full(props.shape[0], 100.0)
+
+        failed = []
+
+        for i in range(6):
+            failed = solve.failed(
+                0,
+                0,
+                0,
+                loads,
+                props[:, 0],
+                props[:, 1],
+                props[:, 2],
+                props[:, 3],
+                1 / 8,
+            )
+
+            loads = np.where(failed, loads - step, loads + step)
+            step /= 2
+
+        loads = np.where(failed, loads - step, loads)
+        print(loads)
+
+        adj = np.zeros(cs_array.shape[0])
+        solve.count_adjacent(cs_array, adj)
+
+        stacked = np.zeros(cs_array.shape[0])
+        solve.count_stacked(cs_array, stacked)
+
+        n_ribs = 15
+        n_stringers = cs_array.shape[1]
+
+        def_q = solve.deflection_z(awq)
