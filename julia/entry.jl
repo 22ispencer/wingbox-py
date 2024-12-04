f66cd4593342026e1b21bc9606a365ce732fc3ff
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2024-12-04 15:46:42 -0600

    ported all functions

--- julia/entry.jl
+++ julia/entry.jl
@@ -1,3 +1,31 @@
 include("solve.jl")
+import Base.iterate, Base.length
 
-const positions = collect(range(0.0, 8.0, step=(1 / 8)))
+struct Combinations{T}
+    itr::Vector{T}
+    count::Int64
+    itrsize::Int64
+    function Combinations(itr::Vector{T}, count::Int) where {T}
+        new{T}(itr, Int64(count), length(itr))
+    end
+end
+
+function iterate(c::Combinations, state::Int64=0)
+    if state >= length(c)
+        return nothing
+    end
+    indices = digits(state, base=c.itrsize, pad=c.count)
+    [c.itr[i] for i in (indices .+ 1)], state + 1
+end
+
+function length(c::Combinations)
+    length(c.itr)^c.count
+end
+
+const stringer_positions = collect(range(0.0, 8.0, step=(1 / 8)))[1:end-1]
+
+function position_combos()
+    collect(Combinations(stringer_positions, 2))
+end
+
+@time position_combos()
--- julia/solve.jl
+++ julia/solve.jl
@@ -54,7 +54,7 @@ function section_properties(stringer_locs)
         end
     end
 
-    y_bar = sum(E .* A .* y) / sum(E .* A)
+    y_bar = esum(E .* A .* y) / sum(E .* A)
     z_bar = sum(E .* A .* z) / sum(E .* A)
     return (
         y_bar=y_bar,
@@ -63,4 +63,84 @@ function section_properties(stringer_locs)
         Izz=sum(E .* (Izz .+ A .* (y .- y_bar) .^ 2)),
         Iyz=sum(E .* (Iyz .+ A .* (y .- y_bar) .* (z .- z_bar))),
     )
+end
+
+
+function deflection_y(mod_elastic, i_yy, i_zz, i_yz, load)
+    return (i_yy / (mod_elastic .* (i_yy .* i_zz .- i_yz .^ 2))) .* (load .- 5) .* -32180.8333333
+end
+
+function deflection_z(mod_elastic, i_yy, i_zz, i_yz, load)
+    return (i_zz / (mod_elastic .* (i_yy .* i_zz .- i_yz .^ 2))) .* (load .- 5) .* -32180.8333333
+end
+
+function shear_flow(load, y_bar)
+    return (load .* (y_bar .- 1) .+ 5 .* y_bar - 10) ./ 4.136
+end
+
+function torsion(x, load, y_bar)
+    return 0.00018401233 .* x .* shear_flow(load, y_bar)
+end
+
+function stress_shear(load, y_bar, thickness)
+    return (load .* (y_bar .- 1) .+ 5 .* y_bar .- 10) ./ (4.136 .* thickness)
+end
+
+function stress_normal(x, y, z, load, i_yy, i_zz, i_yz)
+    return .-y .* ((load .- 5) .* (x .- 45.75) .* i_yy ./ (i_yy .* i_zz .- i_yz .^ 2)) .+ z .* (
+        (load .- 5) .* (x .- 45.75) .* i_zz ./ (i_yy .* i_zz .- i_yz .^ 2)
+    )
+end
+
+function failed()
+    shear = stress_shear(load, y_bar, thickness)
+    normal = stress_normal(x, y, z, load, i_yy, i_zz, i_yz)
+
+    f_1 = -0.038
+    f_11 = 0.0096
+    f_66 = 0.44
+
+    if thickness < 1 / 8
+        f_1 = -0.054
+        f_11 = 0.0072
+    end
+
+    if thickness < 1 / 16
+        f_1 = -0.048
+        f_11 = 0.0078
+        f_66 = 0.31
+    end
+
+    return f_1 .* normal .+ f_11 .* normal .^ 2 .+ f_66 .* shear .^ 2 >= 1
+end
+
+function count_stacked(stringer_locs)
+    sum([count(==(loc), stringer_locs) for loc in unique(stringer_locs)]) - size(stringer_locs, 1)
+end
+
+function count_adjacent(stringer_locs)
+    uniq = unique(stringer_locs)
+    dup = circshift(uniq, 1)
+    return sum(uniq - dup .<= 1 / 8)
+end
+
+function final_score(n_stringer, n_ribs, n_stacked, n_adj, load, def_q, def_max, twist)
+    weight = 0.4369 .+ n_ribs .* 0.00213 .+ n_stringer .* 0.0146
+    design = 100 .* (
+        0.6 .* 8 ./ (n_stringer .+ 1)
+        .+
+        0.3 .* 15 ./ (n_ribs .+ 1)
+        .-
+        (n_stacked ./ 8 .+ n_adj ./ 8)
+    )
+    perf = (
+        0.5 .* load ./ weight
+        .+
+        0.5 ./ def_q
+        .+
+        0.05 .* (load ./ def_max .+ load ./ twist)
+        .-
+        10 .* weight
+    )
+    return design .+ perf
 end
